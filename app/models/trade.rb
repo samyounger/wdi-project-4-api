6900a2502e008e4b57f00585f0dd9def985fe6bc
samyounger <sam_younger@hotmail.com>
2016-10-27 20:54:36 +0100

    no changes, commit before making minor alterations to front-end

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,7 +3,7 @@ class Trade < ApplicationRecord
   validates :user_id, presence: true
   enum trade_type: [:sell, :buy]
 
-  before_validation :check_number_of_shares
+  # before_validation :check_number_of_shares
   before_save :make_amount_negative
 
 
@@ -12,10 +12,14 @@ class Trade < ApplicationRecord
       self.number_of_shares *= -1 if self.trade_type === "sell"
     end
 
-    # Could be tidier
-    def check_number_of_shares
-      if self.user.trades.where(epic: self.epic, trade_type: "sell").map(&:number_of_shares).reduce(:+) < self.number_of_shares.abs
-        errors.add(:number_of_shares, "you don't have enough shares")
-      end
-    end
+    # # Could be tidier
+    # def check_number_of_shares
+    #   if (!shares_of_type < self.number_of_shares.abs) || (shares_of_type.map(&:number_of_shares).reduce(:+) < self.number_of_shares.abs)
+    #     errors.add(:number_of_shares, "you don't have enough shares")
+    #   end
+    # end
+    #
+    # def shares_of_type
+    #   self.user.trades.where(epic: self.epic, trade_type: "buy")
+    # end
 end
