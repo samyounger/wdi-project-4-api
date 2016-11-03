e549386df145f80e7653fafef3bab3a046cd1aae
samyounger <sam_younger@hotmail.com>
2016-11-03 11:42:39 +0000

    moved the lib file to App

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,8 @@ module BackEnd
     config.api_only = true
     config.autoload_paths << Rails.root.join('lib')
 
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    config.autoload_paths += Dir["#{config.root}/app/models/**/"]
+    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    # config.autoload_paths += Dir["#{config.root}/app/models/**/"]
 
     # config.autoload_paths += %W(#{config.root}/lib)
 
