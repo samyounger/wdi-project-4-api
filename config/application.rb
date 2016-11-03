016a99293a3f6ac026bd6751228f8e0eb4b066ff
samyounger <sam_younger@hotmail.com>
2016-11-03 11:11:27 +0000

    removed previous change

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module BackEnd
     config.api_only = true
     config.autoload_paths << Rails.root.join('lib')
 
-    config.autoload_paths += %W(#{config.root}/lib)
+    # config.autoload_paths += %W(#{config.root}/lib)
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
