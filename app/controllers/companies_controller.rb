53c8fffa509c3fdf7ae46b22ffdf52fbd50f391a
Sam Younger <sam.younger@gmail.com>
2016-11-10 19:39:01 +0000

    added set_user to the companies controller, trying to erradicate the cannot read token error

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  before_action :set_user
+
   def searchname
     @searchName = params[:input]
     response = HTTParty.get('http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?count=3&input=' + @searchName)
@@ -10,5 +12,12 @@ class CompaniesController < ApplicationController
     response = HTTParty.get('http://finance.google.com/finance/info?client=ig&q=' + @companyEpic)
     response.sub! '// ', ''
     render json: response.parsed_response
+  end
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
   end
 end
